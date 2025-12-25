32ae813706f1f299e5a4ab405d1611c31c334cdb
Aashish Thapa <aashishthapa520@gmail.com>
2025-12-24 19:04:05 -0600

    refactor(network): migrate to NetworkManager

--- src/mode/station/network.rs
+++ src/mode/station/network.rs
@@ -1,9 +1,8 @@
 use anyhow::Result;
-use iwdrs::{
-    error::{IWDError, network::ConnectError},
-    network::{Network as iwdNetwork, NetworkType},
-};
+use std::sync::Arc;
 use tokio::sync::mpsc::UnboundedSender;
+
+use crate::nm::{AccessPointInfo, NMClient, SecurityType};
 
 use crate::{
     event::Event,
@@ -13,58 +12,96 @@ use crate::{
 
 #[derive(Debug, Clone)]
 pub struct Network {
-    pub n: iwdNetwork,
+    pub client: Arc<NMClient>,
+    pub device_path: String,
+    pub ap_path: String,
     pub name: String,
-    pub network_type: NetworkType,
+    pub network_type: SecurityType,
     pub is_connected: bool,
     pub known_network: Option<KnownNetwork>,
+    pub signal_strength: u8,
 }
 
 impl Network {
-    pub async fn new(n: iwdNetwork) -> Result<Self> {
-        let name = n.name().await?;
-        let network_type = n.network_type().await?;
-        let is_connected = n.connected().await?;
-        let known_network = {
-            match n.known_network().await {
-                Ok(v) => match v {
-                    Some(net) => Some(KnownNetwork::new(net).await.unwrap()),
-                    None => None,
-                },
-                Err(_) => None,
-            }
-        };
-
-        Ok(Self {
-            n,
-            name,
-            network_type,
+    pub fn from_access_point(
+        client: Arc<NMClient>,
+        device_path: String,
+        ap_info: AccessPointInfo,
+        known_network: Option<KnownNetwork>,
+        is_connected: bool,
+    ) -> Self {
+        Self {
+            client,
+            device_path,
+            ap_path: ap_info.path,
+            name: ap_info.ssid,
+            network_type: ap_info.security,
             is_connected,
             known_network,
-        })
+            signal_strength: ap_info.strength,
+        }
     }
 
-    pub async fn connect(&self, sender: UnboundedSender<Event>) -> Result<()> {
-        match self.n.connect().await {
-            Ok(()) => Notification::send(
-                format!("Connected to {}", self.name),
-                NotificationLevel::Info,
-                &sender,
-            )?,
-            Err(e) => match e {
-                IWDError::OperationError(e) => match e {
-                    ConnectError::Aborted => {
-                        Notification::send(e.to_string(), NotificationLevel::Info, &sender)?;
-                    }
-                    _ => {
-                        Notification::send(e.to_string(), NotificationLevel::Error, &sender)?;
-                    }
-                },
-                _ => {
-                    Notification::send(e.to_string(), NotificationLevel::Error, &sender)?;
+    pub async fn connect(&self, sender: UnboundedSender<Event>, password: Option<&str>) -> Result<()> {
+        // Check if we have a saved connection for this network
+        if let Some(known) = &self.known_network {
+            // Use existing connection profile
+            match self
+                .client
+                .activate_connection(&known.connection_path, &self.device_path)
+                .await
+            {
+                Ok(_) => {
+                    Notification::send(
+                        format!("Connecting to {}", self.name),
+                        NotificationLevel::Info,
+                        &sender,
+                    )?;
                 }
-            },
+                Err(e) => {
+                    Notification::send(
+                        format!("Failed to connect: {}", e),
+                        NotificationLevel::Error,
+                        &sender,
+                    )?;
+                }
+            }
+        } else {
+            // Create new connection
+            match self
+                .client
+                .add_and_activate_connection(&self.device_path, &self.ap_path, password)
+                .await
+            {
+                Ok(_) => {
+                    Notification::send(
+                        format!("Connecting to {}", self.name),
+                        NotificationLevel::Info,
+                        &sender,
+                    )?;
+                }
+                Err(e) => {
+                    // Check if password is required
+                    if self.network_type.requires_password() && password.is_none() {
+                        // Password required - this will be handled by the auth flow
+                        return Err(anyhow::anyhow!("Password required"));
+                    }
+                    Notification::send(
+                        format!("Failed to connect: {}", e),
+                        NotificationLevel::Error,
+                        &sender,
+                    )?;
+                }
+            }
         }
         Ok(())
+    }
+
+    pub fn requires_password(&self) -> bool {
+        self.known_network.is_none() && self.network_type.requires_password()
+    }
+
+    pub fn is_enterprise(&self) -> bool {
+        self.network_type.is_enterprise()
     }
 }
