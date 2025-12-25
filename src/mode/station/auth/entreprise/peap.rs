47a9a3e91b4cdc64013c614cf6f241a1d30b981c
Aashish Thapa <aashishthapa520@gmail.com>
2025-12-24 19:04:40 -0600

    refactor(peap): migrate to NetworkManager connection format

--- src/mode/station/auth/entreprise/peap.rs
+++ src/mode/station/auth/entreprise/peap.rs
@@ -12,10 +12,7 @@ use ratatui::{
 
 use tui_input::backend::crossterm::EventHandler;
 
-use crate::{
-    iwd_network_name,
-    mode::station::auth::entreprise::{ERROR_PADDING, UserInputField},
-};
+use crate::mode::station::auth::entreprise::{ERROR_PADDING, UserInputField};
 
 fn pad_string(input: &str, length: usize) -> String {
     let current_length = input.chars().count();
@@ -185,92 +182,80 @@ impl PEAP {
 
     pub fn apply(&mut self, network_name: &str) -> Result<()> {
         self.validate()?;
-        let network_name = iwd_network_name(network_name);
+
+        // NetworkManager stores 802.1x configuration in connection profiles
+        // at /etc/NetworkManager/system-connections/
+        let nm_path = format!(
+            "/etc/NetworkManager/system-connections/{}.nmconnection",
+            network_name
+        );
+
         let mut file = OpenOptions::new()
             .write(true)
             .read(true)
             .create(true)
             .truncate(true)
-            .open(format!("/var/lib/iwd/{}.8021x", network_name))?;
+            .open(&nm_path)?;
 
         let mut text = format!(
-            "
-[Security]
-EAP-Method=PEAP
-EAP-Identity={}
+            "[connection]
+id={}
+type=wifi
+
+[wifi]
+ssid={}
+
+[wifi-security]
+key-mgmt=wpa-eap
+
+[802-1x]
+eap=peap
+identity={}
+phase2-auth={}
+password={}
 ",
-            self.identity.field.value()
+            network_name,
+            network_name,
+            self.identity.field.value(),
+            self.phase2_method.to_string().to_lowercase(),
+            self.phase2_password.value()
         );
 
-        if !self.server_domain_mask.is_empty() {
-            text.push_str(
-                format!(
-                    "EAP-PEAP-ServerDomainMask={}
-",
-                    self.server_domain_mask.value()
-                )
-                .as_str(),
-            );
-        }
-
         if !self.ca_cert.is_empty() {
-            text.push_str(
-                format!(
-                    "EAP-PEAP-CACert={}
-",
-                    self.ca_cert.field.value()
-                )
-                .as_str(),
-            );
+            text.push_str(&format!("ca-cert={}\n", self.ca_cert.field.value()));
         }
 
         if !self.client_cert.is_empty() {
-            text.push_str(
-                format!(
-                    "EAP-PEAP-ClientCert={}
-",
-                    self.client_cert.field.value()
-                )
-                .as_str(),
-            );
+            text.push_str(&format!(
+                "client-cert={}\n",
+                self.client_cert.field.value()
+            ));
         }
 
         if !self.client_key.is_empty() {
-            text.push_str(
-                format!(
-                    "EAP-PEAP-ClientKey={}
-",
-                    self.client_key.field.value()
-                )
-                .as_str(),
-            );
+            text.push_str(&format!(
+                "private-key={}\n",
+                self.client_key.field.value()
+            ));
         }
 
         if !self.key_passphrase.is_empty() {
-            text.push_str(
-                format!(
-                    "EAP-PEAP-ClientKeyPassphrase={}
-",
-                    self.key_passphrase.field.value()
-                )
-                .as_str(),
-            );
+            text.push_str(&format!(
+                "private-key-password={}\n",
+                self.key_passphrase.field.value()
+            ));
         }
 
         text.push_str(
-            format!(
-                "EAP-PEAP-Phase2-Method={}
-EAP-TTLS-Phase2-Identity={}
-EAP-TTLS-Phase2-Password={}
+            "
+[ipv4]
+method=auto
+
+[ipv6]
+method=auto
 ",
-                self.phase2_method,
-                self.phase2_identity.value(),
-                self.phase2_password.value()
-            )
-            .as_str(),
         );
 
-        let text = text.trim_start();
         file.write_all(text.as_bytes())?;
 
         Ok(())
